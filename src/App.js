dee18d4df302252eb8c67811557173bfd0984fb2
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-08-06 09:11:12 +0900

    Reset file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Community from "./Community";
-import Scheduled from "./Scheduled";
-import { GlobalStyle } from "./style";
-import ToDo from "./ToDo";
-import Home from "./Home";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import { useState } from "react";
-import Login from "./Login";
-
 function App() {
-  const [login, setLogin] = useState(false);
-  return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Header />
-      <Sidebar login={login} setLogin={setLogin} />
-      <Routes>
-        <Route element={<Home />} path="/"></Route>
-        <Route element={<Login setLogin={setLogin} />} path="/login"></Route>
-        <Route element={<Community />} path="/community"></Route>
-        <Route element={<Scheduled />} path="/schedule"></Route>
-        <Route element={<ToDo />} path="/todo" />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>dfd</div>;
 }
 
 export default App;
