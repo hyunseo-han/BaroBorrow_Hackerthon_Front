744b432803bed1a3a9967f88151ddf4ad7657587
hyunseo-han <ols3040015@naver.com>
2022-08-19 01:26:13 +0900

    map으로 수정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailProduct from "./product/DetailProduct";
 import { GlobalStyle } from "./style";
-import ProductList from "./ProductList";
-import Nav from "./Nav";
 import ProductResult from "./product/ProductResult";
 import ProductOwner from "./product/ProductOwner";
 import ProductEnroll from "./product/ProductEnroll";
 import Profile from "./mypage/Profile";
 import ProfileEdit from "./mypage/ProfileEdit";
-import HoldMain from "../src/basket/HoldMain";
-import Option from "./nav/Option";
-import MyBorrow from "./mypage/MyBorrow";
-import MainHome from "./nav/MainHome";
-
+import MainHome from "./Nav/MainHome";
+import SignUp from "./Start/SignUp";
+import Main from "./Start/Main";
+import Login from "./Start/Login";
+import Location from "./Start/Location";
+import RouteLogin from "./RouteLogin";
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <MyBorrow />
+      <Routes>
+        {/* 로그인 */}
+        <Route exact path={"/"} element={<Main />} />
+        <Route exact path={"/SignUp"} element={<SignUp />} />
+        <Route exact path={"/Login"} element={<Login />} />
+        <Route exact path={"/Location"} element={<Location />} />
+        <Route element={<RouteLogin />} path={"/user"}>
+          {/* 물품 */}
+          <Route element={<MainHome />} path={"/user/main"} />
+          <Route element={<DetailProduct />} path={"/user/detail:id"} />
+          <Route element={<ProductOwner />} path={"/user/owner"} />
+          <Route element={<ProductEnroll />} path={"/user/enroll"} />
+          <Route element={<ProductResult />} path={"/user/detail:id/result"} />
+          {/* 마이페이지 */}
+          <Route element={<Profile />} path={"/user/mypage/profile"} />
+          <Route element={<ProfileEdit />} path={"/user/mypage/profileEdit"} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
